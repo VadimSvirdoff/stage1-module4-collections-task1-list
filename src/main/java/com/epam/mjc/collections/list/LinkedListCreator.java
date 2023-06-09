7dd8a7e3133f5cb83da9490e4f141ce2c2a19880
Violetta Prikhno <Violetta_Prikhno@epam.com>
2023-06-09 13:22:35 +0300

    My first commit!

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> result = new LinkedList<>();
+        for(Integer item: sourceList){
+            if(item % 2 == 0){
+                result.addLast(item);
+            }else {
+                result.addFirst(item);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int valueA = calculateFunctionValue(a);
+        int valueB = calculateFunctionValue(b);
+
+        if (valueA == valueB) {
+            return a.compareTo(b);
+        } else {
+            return Integer.compare(valueA, valueB);
+        }
+
+    }
+
+    private int calculateFunctionValue(String s) {
+        int x = Integer.parseInt(s);
+        return 5 * x * x + 3;
     }
 }
